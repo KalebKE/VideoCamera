76907ba063735afbce621dae8acf9584bc9b191c
KalebKE <KalebKE@users.noreply.github.com>
2020-02-16 08:55:39 -0700

    Update AppDelegate.m

--- VideoCamera/AppDelegate.m
+++ VideoCamera/AppDelegate.m
@@ -2,8 +2,6 @@
 //  AppDelegate.m
 //  VideoCamera
 //
-//  Created by Churchill Navigation on 2/17/16.
-//  Copyright © 2016 Churchill Navigation. All rights reserved.
 //
 
 #import "AppDelegate.h"
