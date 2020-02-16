3e4316ef3b5f10c2bc392bfe19563ed3a67c2c86
KalebKE <KalebKE@users.noreply.github.com>
2020-02-16 08:56:01 -0700

    Update ViewController.h

--- VideoCamera/ViewController.h
+++ VideoCamera/ViewController.h
@@ -2,8 +2,6 @@
 //  ViewController.h
 //  VideoCamera
 //
-//  Created by Churchill Navigation on 2/17/16.
-//  Copyright © 2016 Churchill Navigation. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
